6a7c7195d0f70e5e4f3d4e9514e27eef280dea4f
Sanif Shahzad <sanifnaqash88@gmail.com>
2024-04-21 23:54:19 +0530

    Create Added

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -17,5 +17,30 @@ namespace BulkyWeb_MVC.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Name and Display Order cannot match");
+            }
+            if (category.Name == "test")
+            {
+                ModelState.AddModelError("name", "Test is an invalid value");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
